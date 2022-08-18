9a8d5d6aebdf60258370a0d2a00daf0f01d478cc
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-08-18 16:14:32 -0700

    commit tree 76c4698c3077a5a8df57501018bb97877075185a

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+ return `Hi, my name is ${name}.`
+}
+introduction("Daniel");
+
+function introductionWithLanguage(name, language) {
+ return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguage("Daniel", "Javascript");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+ return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+introductionWithLanguageOptional("Gracie");
